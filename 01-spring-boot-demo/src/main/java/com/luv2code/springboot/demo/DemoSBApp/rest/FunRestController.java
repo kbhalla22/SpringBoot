940240b62ddb01f1965e3543481aba6514996f4a
karanbhalla <kbhalla22@gmail.com>
2019-03-25 16:43:45 -0400

    setting up springboot

---
+++ 01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/DemoSBApp/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.luv2code.springboot.demo.DemoSBApp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	//expose a / end point
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Time on the server is" +LocalDateTime.now();
+	}
+}
