895a47d5adc78194fc4c82fe1399d0e40c98859a
karanbhalla <kbhalla22@gmail.com>
2019-03-26 21:58:37 -0400

    Added CRUD operation features

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -8,7 +8,6 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
@@ -45,34 +44,42 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
 	@Override
 	public Employee findById(int theId) {
+
 		// get the current hibernate session
-		Session currentSession=entityManager.unwrap(Session.class);
+		Session currentSession = entityManager.unwrap(Session.class);
 		
-		//get the employee
-		Employee theEmployee= currentSession.get(Employee.class, theId);
+		// get the employee
+		Employee theEmployee =
+				currentSession.get(Employee.class, theId);
+		
+		// return the employee
 		return theEmployee;
 	}
 
 
 	@Override
 	public void save(Employee theEmployee) {
-		// get the current session
-		Session currentSession=entityManager.unwrap(Session.class);
+
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
 		
-		//save employee
+		// save employee
 		currentSession.saveOrUpdate(theEmployee);
-		
-		
 	}
 
 
 	@Override
 	public void deleteById(int theId) {
-		// TODO Auto-generated method stub
-		Session currentSession=entityManager.unwrap(Session.class);
 		
-		Query theQuery=currentSession.createQuery("delete from Employee where id=:employeeId");
+		// get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+				
+		// delete object with primary key
+		Query theQuery = 
+				currentSession.createQuery(
+						"delete from Employee where id=:employeeId");
 		theQuery.setParameter("employeeId", theId);
+		
 		theQuery.executeUpdate();
 	}
 
--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeService.java
@@ -5,11 +5,13 @@ import java.util.List;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 public interface EmployeeService {
-public List<Employee> findAll();
 
-public Employee findById(int theId);
-
-public void save(Employee theEmployee);
-
-public void deleteById(int theId);
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
 }
